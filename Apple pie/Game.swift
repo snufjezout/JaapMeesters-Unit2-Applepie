ae4435d87b7206e5463ce8c24e4bc9d5ec7e43cb
Snufje zout <jaap_meesters@hotmail.com>
2018-04-20 18:58:04 +0200

    Initial Commit

---
+++ Apple pie/Game.swift
@@ -0,0 +1,34 @@
+//
+//  Game.swift
+//  Apple pie
+//
+//  Created by Jaap Meesters on 20/04/2018.
+//  Copyright © 2018 Jaap Meesters. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word.characters {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
